f4801a72bf456130ac162bf4cdb48c05114ac549
mohammed-abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2025-12-21 14:29:03 +0200

    feat: Add loading UI with a reusable spinner component for app, payments, and users routes.

---
+++ src/components/loading-spinner.tsx
@@ -0,0 +1,7 @@
+import { Loader2 } from "lucide-react";
+
+export default function LoadingSpinner() {
+  return (
+    <Loader2 className="animate-spin absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+  );
+}
