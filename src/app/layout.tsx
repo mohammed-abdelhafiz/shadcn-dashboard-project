75b6d0f66dd35e404a61db1d8ae2c6a7b21bbf1b
mohammed-abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2025-12-21 14:38:38 +0200

    style: Add `relative` class to main layout element.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default async function RootLayout({
         >
           <SidebarProvider defaultOpen={defaultOpen}>
             <AppSidebar />
-            <main className="w-full">
+            <main className="w-full relative">
               <Navbar />
               <div className="px-4">{children}</div>
             </main>
